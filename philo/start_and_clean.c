75486f34bdc65d4cbfe2760cedf1d56ccf6099ea
nachogooda <ngaurama@student.42.fr>
2025-03-08 15:44:12 +0100

    Added pthread protection

--- philo/start_and_clean.c
+++ philo/start_and_clean.c
@@ -6,11 +6,27 @@
 /*   By: ngaurama <ngaurama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 00:01:28 by ngaurama          #+#    #+#             */
-/*   Updated: 2025/02/03 14:39:45 by ngaurama         ###   ########.fr       */
+/*   Updated: 2025/03/08 15:41:28 by ngaurama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void failed_thread(int i, t_simulation *simulation, int choice)
+{
+	int j;
+	
+	j = 0;
+	if (choice == 1)
+		printf("Error: Failed to create philosopher thread\n");
+	else
+		printf("Error: Failed to create monitor thread\n");
+	while (j < i)
+	{
+		pthread_join(simulation->philosophers[j].thread, NULL);
+		j++;
+	}
+}
 
 int	start_threads(t_simulation *simulation)
 {
@@ -23,14 +39,14 @@ int	start_threads(t_simulation *simulation)
 		if (pthread_create(&(simulation->philosophers[i].thread), NULL,
 				philosopher_routine, &simulation->philosophers[i]))
 		{
-			printf("Error: Failed to create philosopher thread\n");
+			failed_thread(i, simulation, 1);
 			return (1);
 		}
 		i++;
 	}
 	if (pthread_create(&monitor_thread, NULL, monitor_simulation, simulation))
 	{
-		printf("Error: Failed to create monitor thread\n");
+		failed_thread(simulation->num_phi, simulation, 2);
 		return (1);
 	}
 	i = -1;
